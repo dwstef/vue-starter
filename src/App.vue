6220c264b6bee4ffdecea9b35f32d171d3cb75b0
Dawid Stefanik <dawidstefanik@gmail.com>
2021-05-29 15:32:12 +0200

    Reużywanie komponentów

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
 		<a @click="logMeOut()">Wyloguj</a>
 	</div>
 	<div v-else>
-		<login-form @login="logMeIn($event)"></login-form>
+		<login-form @login="logMeIn($event)" button-label="Wchodzę"></login-form>
+		<login-form @login="logMeIn($event)" :button-label="'Wleć ' + (2+2)"></login-form>
+		<login-form @login="logMeIn($event)" :button-label="Math.random() < 0.5 ? 'Etykieta A' : 'Etykieta B'"></login-form>
 	</div>
   </div>
 </template>

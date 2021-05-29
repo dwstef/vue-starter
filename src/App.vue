f4f88eb52bc313f1f1a4fcd9f6984ba06bf845c1
Dawid Stefanik <dawidstefanik@gmail.com>
2021-05-29 15:19:35 +0200

    LoginForm

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,33 @@
   <div>
 	<h1>Witaj w systemie do zapisów na zajęcia</h1>
 	<div v-if="authenticatedEmail !=''">
-		<h4> Jesteś zalogowany jako {{ email }} </h4>
+		<h4> Jesteś zalogowany jako {{ authenticatedEmail }} </h4>
 		<a @click="logMeOut()">Wyloguj</a>
 	</div>
 	<div v-else>
-		Wpisz login: 
-		<input type ="text" v-model="email">
-		<button @click="logMeIn()">Wchodzę</button>
+		<login-form @login="logMeIn($event)"></login-form>
 	</div>
   </div>
 </template>
 
 <script>
 import "milligram";
+import LoginForm from "./LoginForm";
 
 export default {
+	components: {LoginForm},
 	data() {
 		return {
-			email: '',
 			authenticatedEmail: ''
 		};
 	},	
 	
 	methods: {
-		logMeIn(){
-			this.authenticatedEmail = this.email;
+		logMeIn(username){
+			this.authenticatedEmail = username;
 		},
 		logMeOut(){
 			this.authenticatedEmail = '';
-			this.email = '';
 		}
 	}
 };
